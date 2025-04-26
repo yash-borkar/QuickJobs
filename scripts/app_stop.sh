70c1227029ff7576dbb509015fa6555e86ff63ea
Yash Borkar <129173298+yash-borkar@users.noreply.github.com>
2025-04-26 11:45:12 +0530

    Update app_stop.sh

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo "Running app_stop.sh"
 
-echo "Stopping QuickJobs Node application if running..."
-
-pm2 stop quickjobs-app || true
-pm2 delete quickjobs-app || true
+# Stop the app if it's already running
+pm2 stop quickjobs || true
+pm2 delete quickjobs || true
